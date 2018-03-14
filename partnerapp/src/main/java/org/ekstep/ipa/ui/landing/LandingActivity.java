6a402126a47ebc07fc4f139fa4b1a58629508119
RandhirGupta <randhir.kumar@tarento.com>
2018-03-14 19:23:40 +0530

    Issue #10 feat : Showing dialog to turn off mobile data if connected to openRAP network.

--- partnerapp/src/main/java/org/ekstep/ipa/ui/landing/LandingActivity.java
+++ partnerapp/src/main/java/org/ekstep/ipa/ui/landing/LandingActivity.java
@@ -1,9 +1,20 @@
 package org.ekstep.ipa.ui.landing;
 
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +26,8 @@ import org.ekstep.genie.base.IPresenterFactory;
 import org.ekstep.ipa.R;
 import org.ekstep.ipa.ui.addchild.AddChildActivity;
 import org.ekstep.ipa.ui.landing.geniechildren.GenieChildrenFragment;
-import org.ekstep.openrap.util.ConnectToOpenRapNetwork;
+
+import static android.net.NetworkInfo.State.CONNECTED;
 
 /**
  * @author vinayagasundar
@@ -23,9 +35,21 @@ import org.ekstep.openrap.util.ConnectToOpenRapNetwork;
 
 public class LandingActivity extends BaseActivity implements LandingContract.View {
 
+    private static final String OPEN_RAP_SSID = "openRAP";
     private LandingContract.Presenter mPresenter;
-    private ConnectToOpenRapNetwork mConnectToOpenRapNetwork;
+    private ConnectivityManager mConnectivityManager;
 
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (!intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)) {
+                if (isEnabled() && isConnectedToOpenRAPNetwork(OPEN_RAP_SSID)) {
+                    showDialogForDisablingMobileData();
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,21 +67,21 @@ public class LandingActivity extends BaseActivity implements LandingContract.Vie
 
         mPresenter.start();
 
-        mConnectToOpenRapNetwork = new ConnectToOpenRapNetwork(LandingActivity.this, this);
-
+        mConnectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        mConnectToOpenRapNetwork.registerReceiver();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(mBroadcastReceiver, intentFilter);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mConnectToOpenRapNetwork.unRegisterReceiver();
+        unregisterReceiver(mBroadcastReceiver);
     }
 
     @Override
@@ -117,4 +141,57 @@ public class LandingActivity extends BaseActivity implements LandingContract.Vie
         return this;
     }
 
+    private void showDialogForDisablingMobileData() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(org.ekstep.openrap.R.string.disable_internet_msg));
+
+        builder.setPositiveButton(getString(org.ekstep.openrap.R.string.dialog_ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                goToNetworkSettingScreen();
+            }
+        });
+
+        builder.setNegativeButton(getString(org.ekstep.openrap.R.string.dialog_cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+    private void goToNetworkSettingScreen() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setComponent(new ComponentName("com.android.settings",
+                    "com.android.settings.Settings$DataUsageSummaryActivity"));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } catch (ActivityNotFoundException ignored) {
+        }
+    }
+
+    public boolean isEnabled() {
+        return ((mConnectivityManager != null) ? mConnectivityManager.getNetworkInfo(0).getState() : 0) == CONNECTED;
+    }
+
+    private boolean isConnectedToOpenRAPNetwork(String ssid) {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (wifiManager != null) {
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (wifiInfo != null) {
+                NetworkInfo.DetailedState state = WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState());
+                String currentSsid = wifiInfo.getSSID();
+                if (currentSsid.equals(String.format("\"%s\"", ssid)) &&
+                        (state == NetworkInfo.DetailedState.CONNECTED || state == NetworkInfo.DetailedState.OBTAINING_IPADDR)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
